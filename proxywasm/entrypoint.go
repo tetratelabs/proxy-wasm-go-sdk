534825b80912e46232401728125ebdeac7b33d2b
Takeshi Yoneda <takeshi@tetrate.io>
2021-06-18 12:27:22 +0900

    more
    Signed-off-by: Takeshi Yoneda <takeshi@tetrate.io>

--- proxywasm/entrypoint.go
+++ proxywasm/entrypoint.go
@@ -5,6 +5,11 @@ import (
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 )
 
+// SetNewRootContextFn is the entrypoint for setting up this entire Wasm VM.
+// The given function is responsible for creating types.RootContext and is called when
+// the host initializes a plugin for each plugin configuration.
+// Please make sure that this entrypoint be called during "main()" function, otherwise
+// this VM would fail.
 func SetNewRootContextFn(f func(contextID uint32) types.RootContext) {
 	internal.SetNewRootContextFn(f)
 }
