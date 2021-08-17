13e4a103e72cf1a1e4dea8b6902754a10a7e7f7c
Kotaro Inoue <k.musaino@gmail.com>
2021-08-17 15:42:03 +0900

    Remove ineffective option

--- e2e/loadtest/load_test.go
+++ e2e/loadtest/load_test.go
@@ -53,7 +53,6 @@ func TestAvailabilityAgainstHighHTTPLoad(t *testing.T) {
 	opts.URL = "http://localhost:18000/uuid"
 	opts.AllowInitialErrors = true
 	opts.NumThreads = runtime.NumCPU()
-	opts.NumConnections = (int(*qps) * *duration) / 2 // Set huge enough value to avoid connection reset by peer errors
 	opts.Percentiles = []float64{99.0}
 
 	// Set payload (request body) size
@@ -67,7 +66,7 @@ func TestAvailabilityAgainstHighHTTPLoad(t *testing.T) {
 	log.Printf("\tDuration = %d [s], payloadSize = %d [byte]\n", *duration, *payloadSize)
 	opts.QPS = *qps
 	opts.Duration = time.Duration(*duration) * time.Second
-	
+
 	results, err := fhttp.RunHTTPTest(&opts)
 
 	// Profile final memory usage of the envoy process after executing load testing scenario
@@ -78,7 +77,7 @@ func TestAvailabilityAgainstHighHTTPLoad(t *testing.T) {
 	log.Printf("\tinitial memory status(allocated/heapsize): %d/%d\n", initialMemoryStat.Allocated, initialMemoryStat.HeapSize)
 	log.Printf("\tfinal memory status(allocated/heapsize): %d/%d (increased %f%%)\n", finalMemoryStat.Allocated, finalMemoryStat.HeapSize, float64(finalMemoryStat.Allocated-initialMemoryStat.Allocated)/float64(initialMemoryStat.Allocated)*100)
 	fortioLog.WriteTo(log.Writer())
-	
+
 	successRate := float64(results.RetCodes[200]) / float64(results.DurationHistogram.Count)
 	require.GreaterOrEqual(t, successRate, targetSuccessRate, stdErr.String())
 	require.LessOrEqual(t, results.DurationHistogram.Percentiles[0].Value, float64(targetNintyninthPercentileLatencyLimit), stdErr.String())
