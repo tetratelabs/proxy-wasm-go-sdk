6a30f5783ed68d828a9433b0e5951390dc9108d0
Kotaro Inoue <k.musaino@gmail.com>
2021-08-18 00:29:23 +0900

    Implement simple jwt-powered authz example

---
+++ examples/http_jwt_auth/main.go
@@ -0,0 +1,120 @@
+// Copyright 2021 Tetrate
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"strings"
+
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+const (
+	secretKey = "secret"
+)
+
+func main() {
+	proxywasm.SetVMContext(&vmContext{})
+}
+
+type vmContext struct {
+	// Embed the default VM context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultVMContext
+}
+
+// Override types.DefaultVMContext.
+func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
+	return &pluginContext{}
+}
+
+type pluginContext struct {
+	// Embed the default plugin context here,
+	// so that we don't need to reimplement all the methods.
+	types.DefaultPluginContext
+	isAuthorizationMode bool
+}
+
+// Override types.DefaultPluginContext.
+func (ctx *pluginContext) NewHttpContext(contextID uint32) types.HttpContext {
+	return &httpContext{contextID: contextID}
+}
+
+// Override types.DefaultPluginContext.
+func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
+	data, err := proxywasm.GetPluginConfiguration()
+	if err != nil {
+		proxywasm.LogCriticalf("error reading plugin configuration: %v", err)
+	}
+	ctx.isAuthorizationMode = string(data) == "authorization"
+	return types.OnPluginStartStatusOK
+}
+
+type httpContext struct {
+	// mbed the default plugin context
+	// so that you don't need to reimplement all the methods by yourself.
+	types.DefaultHttpContext
+	contextID uint32
+}
+
+// Override types.DefaultHttpContext.
+func (ctx *httpContext) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
+	authorization, err := proxywasm.GetHttpRequestHeader("Authorization")
+	if err != nil {
+		if err := proxywasm.SendHttpResponse(400, nil, []byte("authorization header must be provided")); err != nil {
+			panic(err)
+		}
+		return types.ActionPause
+	}
+
+	proxywasm.LogInfof("authorization token: %s", authorization)
+
+	slice := strings.Fields(authorization)
+	if len(slice) != 2 || slice[0] != "Bearer" || !validateToken(slice[1]) {
+		if err := proxywasm.SendHttpResponse(401, nil, []byte("invalid token")); err != nil {
+			panic(err)
+		}
+		return types.ActionPause
+	}
+
+	proxywasm.LogInfof("request authorized!")
+
+	return types.ActionContinue
+}
+
+// Override types.DefaultHttpContext.
+func (ctx *httpContext) OnHttpStreamDone() {
+	proxywasm.LogInfof("%d finished", ctx.contextID)
+}
+
+// validateToken checks if the JWT token is valid.
+func validateToken(token string) bool {
+	slice := strings.Split(token, ".")
+	if len(slice) != 3 {
+		return false
+	}
+	unsignedToken := strings.Join(slice[:2], ".")
+	signature, err := base64.RawURLEncoding.DecodeString(slice[2])
+	if err != nil {
+		return false
+	}
+	mac := hmac.New(sha256.New, []byte(secretKey))
+	mac.Write([]byte(unsignedToken))
+	expectedSignature := mac.Sum(nil)
+	return hmac.Equal(signature, expectedSignature)
+}
---
+++ examples/http_jwt_auth/main_test.go
@@ -0,0 +1,98 @@
+// These tests are supposed to run with `proxytest` build tag, and this way we can leverage the testing framework in "proxytest" package.
+// The framework emulates the expected behavior of Envoyproxy, and you can test your extensions without running Envoy and with
+// the standard Go CLI. To run tests, simply run
+// go test -tags=proxytest ./...
+
+//+build proxytest
+
+package main
+
+import (
+	"testing"
+	"fmt"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/proxytest"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+const (
+	sampleJwtToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M`
+)
+
+func TestHttpJwtAuth_OnHttpRequestHeaders(t *testing.T) {
+	opt := proxytest.NewEmulatorOption().WithVMContext(&vmContext{})
+	host, reset := proxytest.NewHostEmulator(opt)
+	defer reset()
+
+	t.Run("success authorization", func(t *testing.T) {
+		// Create http context.
+		id := host.InitializeHttpContext()
+
+		// Call OnRequestHeaders.
+		action := host.CallOnRequestHeaders(id, [][2]string{
+			{"Authorization", fmt.Sprintf("Bearer %s", sampleJwtToken)},
+		}, false)
+
+		// Must be continued.
+		require.Equal(t, types.ActionContinue, action)
+
+		// Call OnHttpStreamDone
+		host.CompleteHttpContext(id)
+
+		// Check Envoy logs.
+		logs := host.GetInfoLogs()
+		require.Contains(t, logs, fmt.Sprintf("%d finished", id))
+	})
+
+	t.Run("400 response", func(t *testing.T) {
+		// Create http context.
+		id := host.InitializeHttpContext()
+
+		// Call OnRequestHeaders.
+		action := host.CallOnRequestHeaders(id, nil, false)
+
+		// Must be paused.
+		require.Equal(t, types.ActionPause, action)
+
+		// Check the local response.
+		localResponse := host.GetSentLocalResponse(id)
+		require.NotNil(t, localResponse)
+		require.Equal(t, uint32(400), localResponse.StatusCode)
+		require.Equal(t, "authorization header must be provided", string(localResponse.Data))
+
+		// Call OnHttpStreamDone
+		host.CompleteHttpContext(id)
+
+		// Check Envoy logs.
+		logs := host.GetInfoLogs()
+		require.Contains(t, logs, fmt.Sprintf("%d finished", id))
+	})
+
+	t.Run("401 response for invalid token", func(t *testing.T) {
+		// Create http context.
+		id := host.InitializeHttpContext()
+
+		// Call OnRequestHeaders.
+		action := host.CallOnRequestHeaders(id, [][2]string{
+			{"Authorization", "invalidtoken"},
+		 }, false)
+
+		// Must be paused.
+		require.Equal(t, types.ActionPause, action)
+
+		// Check the local response.
+		localResponse := host.GetSentLocalResponse(id)
+		require.NotNil(t, localResponse)
+		require.Equal(t, uint32(401), localResponse.StatusCode)
+		require.Equal(t, "invalid token", string(localResponse.Data))
+
+		// Call OnHttpStreamDone
+		host.CompleteHttpContext(id)
+
+		// Check Envoy logs.
+		logs := host.GetInfoLogs()
+		require.Contains(t, logs, fmt.Sprintf("%d finished", id))
+	})
+}
