a2c0e12909712a86d057398ec42b998349439d0c
Kotaro Inoue <k.musaino@gmail.com>
2021-08-22 19:57:14 +0900

    Move testutil to subpackage

--- e2e/testutil/testutil.go
+++ e2e/testutil/testutil.go
@@ -11,20 +11,27 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package e2e
+package testutil
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+)
+
+var (
+	memoryProfileRate = 100 // hz
 )
 
 func CheckMessage(str string, exps, nexps []string) bool {
@@ -70,12 +77,19 @@ func StartEnvoy(t *testing.T, adminPort int) (stdErr *bytes.Buffer, kill func())
 }
 
 // MemoryStat represents the response format of :8081/memory which returns memory stat for envoy process.
+// The fields are defined in https://www.envoyproxy.io/docs/envoy/latest/api-v3/admin/v3/memory.proto.html.
 type MemoryStat struct {
+	// The number of bytes allocated by the heap for envoy.
 	Allocated          memoryBytes `json:"allocated"`
+	// The number of bytes reserved for the heap.
 	HeapSize           memoryBytes `json:"heap_size"`
+	// The number of bytes in free, unmapped pages in the page heap.
 	PageheapUnmapped   memoryBytes `json:"pageheap_unmapped"`
+	// The number of bytes in free, mapped pages in the page heap.
 	PageheapFree       memoryBytes `json:"pageheap_free"`
+	// The amount of memory used by the TCMalloc thread caches.
 	TotalThreadCache   memoryBytes `json:"total_thread_cache"`
+	// The number of bytes of the physical memory usage by the allocator.
 	TotalPhysicalBytes memoryBytes `json:"total_physical_bytes"`
 }
 
@@ -95,16 +109,93 @@ func (m *memoryBytes) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+var memoryStats struct {
+	sync.Mutex
+	memstats []MemoryStat
+}
+
+// EnvoyMemoryProfile represents a memory profile of envoy process.
+type EnvoyMemoryProfile struct {
+	mu sync.Mutex
+	profiling bool
+	ctx context.Context
+	cancel func()
+	done chan bool
+}
+
+var envoyMemoryProfile EnvoyMemoryProfile
+
+// StartEnvoyMemoryProfile starts the memory profiling of envoy process.
+func StartEnvoyMemoryProfile(adminPort int) error {
+	envoyMemoryProfile.mu.Lock()
+	defer envoyMemoryProfile.mu.Unlock()
+	if envoyMemoryProfile.done == nil {
+		envoyMemoryProfile.done = make(chan bool)
+	}
+	if envoyMemoryProfile.ctx == nil {
+		ctx := context.Background()
+		envoyMemoryProfile.ctx, envoyMemoryProfile.cancel = context.WithCancel(ctx)
+	}
+	if envoyMemoryProfile.profiling {
+		return fmt.Errorf("profiling is already running")
+	}
+	envoyMemoryProfile.profiling = true
+	go runProfile(envoyMemoryProfile.ctx, adminPort)
+	return nil
+}
+
+// runProfile is profiling the memory usage of envoy process for every 100ms.
+func runProfile(ctx context.Context, adminPort int) {
+	var err error
+	for {
+		time.Sleep(100 * time.Millisecond)
+		var m *MemoryStat
+		m, e := EnvoyMemoryUsage(adminPort)
+		if e != nil {
+			err = e
+		}
+		memoryStats.Lock()
+		memoryStats.memstats = append(memoryStats.memstats, *m)
+		memoryStats.Unlock()
+		select {
+		case <-ctx.Done():
+			if err != nil {
+				log.Fatal(err)
+			}
+			envoyMemoryProfile.done <- true
+		default:
+
+		}
+	}
+}
+
+// StopEnvoyMemoryProfile stops the memory profiling of envoy process.
+func StopEnvoyMemoryProfile() ([]MemoryStat, error) {
+	envoyMemoryProfile.mu.Lock()
+	defer envoyMemoryProfile.mu.Unlock()
+	if !envoyMemoryProfile.profiling {
+		return nil, fmt.Errorf("profiling is not running")
+	}
+	envoyMemoryProfile.profiling = false
+	envoyMemoryProfile.cancel()
+	<-envoyMemoryProfile.done
+
+	memoryStats.Lock()
+	defer memoryStats.Unlock()
+	memstats := memoryStats.memstats
+	memoryStats.memstats = make([]MemoryStat, 0)
+	return memstats, nil
+}
+
 // EnvoyMemoryUsage is used for getting the memory usage of envoy process.
-func EnvoyMemoryUsage(t *testing.T, adminPort int) MemoryStat {
+func EnvoyMemoryUsage(adminPort int) (*MemoryStat, error) {
 	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/memory", adminPort))
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	require.NoError(t, err)
 	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
 
 	var memory MemoryStat
-	if err := json.NewDecoder(resp.Body).Decode(&memory); err != nil {
-		require.NoError(t, err)
-	}
-	return memory
+	err = json.NewDecoder(resp.Body).Decode(&memory)
+	return &memory, err
 }
