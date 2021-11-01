6959997a4dc7f33482c23eac58edf2f04ad68276
Kotaro Inoue <k.musaino@gmail.com>
2021-11-01 11:08:23 +0900

    Update examples/http_jwt_auth/main_test.go
    Co-authored-by: Takeshi Yoneda <yoneda.takeshi.md@alumni.tsukuba.ac.jp>

--- examples/http_jwt_auth/main_test.go
+++ examples/http_jwt_auth/main_test.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	// sampleJwtToken is a sample JWT token which is already signed with secret key "secret".
+	// A sample JWT token which is already signed with secret key "secret".
 	sampleJwtToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.t-IDcSemACt8x4iTMCda8Yhe3iZaWbvV5XKSTbuAn0M`
 )
 
